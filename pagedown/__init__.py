4ee1e2505000075779d844869b83ae1ac4c03ae6
sharathchandra0007 <kilaparthisarathkumar@gmail.com>
2025-11-15 15:18:44 +0530

    Update __init__.py

--- pagedown/__init__.py
+++ pagedown/__init__.py
@@ -1,6 +1,9 @@
-import django
-
 VERSION = ('2', '2', '1')
 
-if django.VERSION < (3, 2):
-    default_app_config = 'pagedown.apps.PagedownConfig'
+try:
+    import django
+    if django.VERSION < (3, 2):
+        default_app_config = 'pagedown.apps.PagedownConfig'
+except ImportError:
+    # Django is not available during build (pip install .)
+    pass
